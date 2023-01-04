e026ebb1230da12c3498e67206a5f97a2265db0c
harsh <harsh@HARSHAD>
2023-01-04 13:08:43 +0530

    bgh nit

--- XYZ_git/src/A.java
+++ XYZ_git/src/A.java
@@ -1,14 +1,20 @@
 
-public class A {
+public class A 
+{
 
 	public void m1()
 	{
 	 System.out.println("Tula kai yet nai");
 	}
+	public void m2()
+	{
+		System.out.println("Tula lai yet ka??");
+	}
 	public static void main(String[] args) 
 	{
 		A a=new A();
 		a.m1();
+		a.m2();
           System.out.println("HELLOOOOO");
 	}
 
